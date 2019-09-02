923aa874cc5e97ddcc011ebe0369fa45d80a7fd5
Italo Mota <italo.mota@atoscapital.com.br>
2019-09-02 18:26:06 -0300

    Inicia implementação do branch dev

--- src/socketServer.ts
+++ src/socketServer.ts
@@ -1,6 +1,6 @@
 enum Modulo {
   CHAT = 'CHAT',
-  DADOS_ACESSO = 'DADOS_ACESSO',
+  PORTAL = 'PORTAL',
 }
 
 enum Acao {
@@ -10,60 +10,27 @@ enum Acao {
   DELETE = 'DELETE',
 }
 
-interface ObjetoPadrao {
-  tipo: string;
-  token: string;
-  cdRede: number;
-  cdGrupoEmpresa: number;
-  cnpj: string;
-  idUsuario: number;
-  idConexao: string;
+interface Information {
+  tipo?: string;
+  token?: string;
+  cdRede?: number;
+  cdGrupoEmpresa?: number;
+  cnpj?: string;
+  idUsuario?: number;
+  idConexao?: string;
   evento: {
     modulo: Modulo;
     acao: Acao;
   };
-  filtro: object;
-  dados: object;
+  filtro?: object;
+  dados?: object;
 }
 
 class SocketServer {
-  public ativar (io): void {
+  public ativar (io: SocketIO.Server): void {
     io.on('connect', socket => {
-      socket.on('entrarSala', sala => {
-        socket.join(sala, () => {
-          socket.emit('sala')
-        })
-      })
-
-      socket.on('sairSala', sala => {
-        socket.leave(sala, () => {
-          socket.emit('sala')
-        })
-      })
-
-      socket.on('novaMensagem', novaMensagem => {
-        socket.broadcast.emit('mensagens', novaMensagem)
-      })
-
-      socket.on('novaMensagemSala', novaMensagem => {
-        io.sockets.in('1107').emit('mensagens', novaMensagem)
-      })
-
-      socket.on('novaMensagemId', (id, novaMensagem) => {
-        io.sockets.in(id).emit('mensagens', novaMensagem)
-      })
-
-      socket.on('consultarTelaDadosAcesso', info => {
-        info.dados.Registros = [{ login: 'teste', senha: 'teste', status: true }]
-        io.sockets.in('1107').emit('consultarTelaDadosAcessoResp', info)
-      })
-
-      socket.on('solicitacaoServidor', (info: ObjetoPadrao) => {
-        if (info.tipo === Modulo.CHAT) {
-          socket.broadcast.emit('respSolicitacaoServidor', info)
-        } else {
-          socket.broadcast.emit('respSolicitacaoServidor', info)
-        }
+      socket.on('request', (info: Information) => {
+        socket.broadcast.emit('response', info)
       })
     })
   }
