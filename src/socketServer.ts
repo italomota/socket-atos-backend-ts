37be122ed63b01f88ec0bcd5c44cbad5f0cc5141
Italo Mota <italo.mota@atoscapital.com.br>
2019-09-04 18:01:29 -0300

    Atualiza backend

--- src/socketServer.ts
+++ src/socketServer.ts
@@ -1,8 +1,6 @@
 enum Modulo {
-  LOGIN = 'LOGIN',
   CHAT = 'CHAT',
   DADOS_ACESSO = 'DADOS_ACESSO',
-  RELATORIO_VENDAS = 'RELATORIO_VENDAS',
 }
 
 enum Acao {
@@ -29,38 +27,23 @@ interface Information {
 }
 
 class SocketServer {
-  public autenticarUsuario (info: Information, socket: SocketIO.Socket): void {
-    if (info.evento.dados.usuario && info.evento.dados.senha) {
-      const respostaAPI = {
-        token: 'kml123tbt',
-        cdRede: 110,
-        cdGrupoEmpresa: 137,
-        cnpj: '11111111000120',
-        idUsuario: 10001
-      }
-
-      socket.join(respostaAPI.cdRede)
-      socket.join(respostaAPI.cdGrupoEmpresa)
-      socket.join(respostaAPI.cnpj, () => {
-        console.log(Object.keys(socket.rooms))
-      })
-
-      socket.emit('response', {
-        ...info,
-        token: 'kml123tbt'
-      })
-
-      return
-    }
-    socket.emit('response', info)
-  }
-
   public ativar (io: SocketIO.Server): void {
     io.on('connect', socket => {
-      socket.on('request', (info: Information) => {
+      const { cdRede, cdGrupoEmpresa, cnpj, idUsuario } = socket.handshake.query
+
+      socket.join(cdRede)
+      socket.join(cdGrupoEmpresa)
+      socket.join(cnpj)
+      socket.join(idUsuario)
+
+      socket.on('sendBroadcast', (info: Information) => {
         switch (info.evento.modulo) {
-          case Modulo.LOGIN: {
-            this.autenticarUsuario(info, socket)
+          case (Modulo.CHAT): {
+            io.emit('response', info)
+            break
+          }
+          case (Modulo.DADOS_ACESSO): {
+            io.emit('response', info)
             break
           }
         }
